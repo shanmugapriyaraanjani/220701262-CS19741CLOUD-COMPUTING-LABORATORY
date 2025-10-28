ee76a273a8b96c70d06d8ab38256b27dc4d6dddd
SHANMUGA PRIYA RAANJANI S H <140168503+shanmugapriyaraanjani@users.noreply.github.com>
2025-10-28 22:33:16 +0530

    Update AvaliableRooms.js

--- frontend/src/components/Room/AvaliableRooms.js
+++ frontend/src/components/Room/AvaliableRooms.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import Header from "./Header";
 import "../../App.css";
 import { getAvailableRooms } from "../services";
